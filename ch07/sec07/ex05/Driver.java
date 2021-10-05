1032a0e9c7f36d0e03dd6ba0ee5eefa1273c5215
choemujung <mnw0507@naver.com>
2021-10-05 19:28:03 +0900

    ~328

---
+++ ch07/sec07/ex05/Driver.java
@@ -0,0 +1,7 @@
+package ch07.sec07.ex05;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ ch07/sec07/ex07/Parent.java
@@ -0,0 +1,5 @@
+package ch07.sec07.ex07;
+
+public class Parent {
+
+}
